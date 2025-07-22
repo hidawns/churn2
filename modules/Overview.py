93c3cc27e72c3faf2f3e33f0944a45406098ac53
hidawns <134594505+hidawns@users.noreply.github.com>
2025-07-22 14:06:26 +0700

    Update Overview.py

--- modules/Overview.py
+++ modules/Overview.py
@@ -38,7 +38,7 @@ def show():
             color: #004280;
             line-height: 1.5;
         '>
-            <div style='text-align: center; font-weight: 600; margin-bottom: 8px;'>Kualitas Layanan Buruk</div>
+            <div style='text-align: center; font-weight: 700; margin-bottom: 8px;'>Kualitas Layanan Buruk</div>
             <div style='text-align: justify;'>Kualitas layanan yang tidak memuaskan dapat mendorong pelanggan untuk churn.</div>
         </div>
         """, unsafe_allow_html=True)
